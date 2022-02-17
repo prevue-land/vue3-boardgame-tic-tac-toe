888ae7b0919dc8c9d8aac985bfe3807f92fdbf34
Maciej Pędzich <dnalop.me@gmail.com>
2022-02-17 21:05:19 +0100

    Change About section's title

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,7 +3,7 @@ const currentYear = new Date().getFullYear();
 </script>
 
 <template>
-  <h1>blank-canvas</h1>
+  <h1>vue3-boardgame Tic Tac Toe</h1>
   <span>
     &copy; {{ currentYear }} Weekly OverVue. Released under MIT License
   </span>
