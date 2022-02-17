d2bc7b7d1cdd8be373b2f1c85fc0d78528ca37ff
Maciej Pędzich <dnalop.me@gmail.com>
2022-02-17 21:08:05 +0100

    Write a generic block button component

---
+++ src/components/BlockButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="block-button">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+.block-button {
+  width: 100%;
+  margin-bottom: 1rem;
+  padding: 0.5rem;
+  background-color: #35495e;
+  color: white;
+  border: none;
+  text-align: center;
+  transition: 0.3s;
+}
+
+button:hover {
+  background-color: #42b883;
+  color: #000;
+  outline: none;
+}
+</style>
