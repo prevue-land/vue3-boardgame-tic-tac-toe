dd5353810de3528f3765a7ead92ee97940f28cc1
Maciej Pędzich <dnalop.me@gmail.com>
2022-02-17 21:12:01 +0100

    Add menu button links

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,28 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+import BlockButton from '@/components/BlockButton.vue';
+</script>
+
 <template>
-  <h1>Have fun!</h1>
+  <div id="menu-container">
+    <h1>vue3-boardgame Tic Tac Toe</h1>
+    <div id="menu-buttons">
+      <RouterLink to="/game">
+        <BlockButton>PLAY!</BlockButton>
+      </RouterLink>
+      <a href="https://weeklyovervue.com">
+        <BlockButton>Read the tutorial article</BlockButton>
+      </a>
+      <RouterLink to="/about">
+        <BlockButton>About</BlockButton>
+      </RouterLink>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+#menu-buttons {
+  display: flex;
+  flex-direction: column;
+}
+</style>
